2f6a8350a85b75181cdb6de7675fe7c433cbea00
Hubert Ciebiada <hubert.ciebiada@gmail.com>
2026-01-24 14:20:17 +0100

    Add heating curve page and enhance dashboard functionality

--- src/PumpAhead.Adapters.Web/WebOptions.cs
+++ src/PumpAhead.Adapters.Web/WebOptions.cs
@@ -3,4 +3,6 @@ namespace PumpAhead.Adapters.Web;
 public class WebOptions
 {
     public Guid DefaultSensorId { get; set; } = Guid.Empty;
+    public int ForecastOffsetHours { get; set; } = 6;
+    public double TargetIndoorTemperature { get; set; } = 22.0;
 }
